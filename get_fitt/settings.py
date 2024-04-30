b3abc7188fdfcede300b595f97f28c51f065ef34
Mitchell Jenkins <mitchelljenkins1999@gmail.com>
2024-04-30 11:34:01 +0000

    Remove and update secret key details in heroku

--- get_fitt/settings.py
+++ get_fitt/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%0gtwa9==25#u7$4hx_mo*6y-f&9bvug+m=i8f$@-49ad$97$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'get-fitt-c5544b6e6a49.herokuapp.com', 'localhost'
