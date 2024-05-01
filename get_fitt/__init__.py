843cd3926c0692c4abe7606f341d1c4a627c55e8
Mitchell Jenkins <mitchelljenkins1999@gmail.com>
2024-05-01 08:57:03 +0000

    Update details for deployment

---
+++ get_fitt/__init__.py
@@ -0,0 +1,2 @@
+import os
+env_name = os.getenv('env.py', 'local')
--- get_fitt/settings.py
+++ get_fitt/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY_VALUE', 'x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
-    'get-fitt-c5544b6e6a49.herokuapp.com', 'localhost'
+    'get-fitt-c5544b6e6a49.herokuapp.com', '8000-jenkimdev-getfitt-mgvg3sz7ld9.ws-eu110.gitpod.io'
 ]
 
 
